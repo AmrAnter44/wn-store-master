57c75fa652d8f1b1ea51b311c1035a07f18a5c26
AmrAnter44 <137618860+AmrAnter44@users.noreply.github.com>
2025-09-13 19:57:18 +0300

    ad

--- src/app/Store.jsx
+++ src/app/Store.jsx
@@ -6,7 +6,7 @@ import { supabase } from "@/lib/supabaseClient";
 
 import { FaFilter, FaFilterCircleXmark } from "react-icons/fa6";
 import { useMyContext } from "../context/CartContext";
-import Storepopup from "./product/[id]/Storepopup"; // ✅
+
 
 export default function StorePage() {
   const { addToCart } = useMyContext();
@@ -232,12 +232,10 @@ export default function StorePage() {
                 </Link>
 
                 {/* ✅ Add to Cart button */}
-                <button
-                  onClick={() => setSelectedProductId(product.id)}
-                  className="bg text-white px-4 py-2 rounded mt-2 hover:bg-gray-800 transition"
-                >
-                  Add to Cart
-                </button>
+
+                <Link      href={`/product/${product.id}`}             className="bg text-white px-4 py-2 rounded mt-2 hover:bg-gray-800 transition">
+                                  Add to Cart
+                </Link>
               </div>
             ))}
           </div>
