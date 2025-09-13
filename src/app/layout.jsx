5e0775d107610dc4225e29b9c189a3e15ddfc289
AmrAnter44 <137618860+AmrAnter44@users.noreply.github.com>
2025-09-13 21:36:49 +0300

    asd

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,7 @@ import { MyContextProvider } from "../context/CartContext";
 import { Outfit } from "next/font/google";
 import Footer from "./Footer";
 import ClientLayoutWrapper from "./ClientLayoutWrapper";
-
+import { Analytics } from "@vercel/analytics/react"
 const outfit = Outfit({ subsets: ["latin"], weight: ["400","500","600","700","800","900"] });
 
 export const metadata = {
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${outfit.className}  h-full`}>
       <body className={`${outfit.className} antialiased  flex flex-col min-h-screen`}>
+        <Analytics />
         <MyContextProvider>
           <ClientLayoutWrapper className = "flex-1">
             {children}
